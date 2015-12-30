6e425cb7704045788b12163f34e519f29280909a
谢志刚 <kw1073kk_xzg@163.com>
2015-12-30 21:05:19 +0800

    【添加】添加豆瓣接口的url配置

--- interests/apps/books/urls.py
+++ interests/apps/books/urls.py
@@ -8,4 +8,9 @@ from apps.books import db_views
 
 urlpatterns = [
     url(r'^get_book_by_id/$', db_views.get_book_by_id),
+    url(r'^get_book_by_isbn/$', db_views.get_book_by_isbn),
+    url(r'^search_book_by_name/$', db_views.search_book_by_name),
+    url(r'^get_book_collect_by_uid/$', db_views.get_book_collect_by_uid),
+    url(r'^get_book_notes_by_id/$', db_views.get_book_notes_by_id),
+    url(r'^get_book_series_by_id/$', db_views.get_book_series_by_id),
 ]
